461e5cf328ffb380da2fa6fe9329f024cba628e9
Andrey Barantsov <andrey.barantsov75@gmail.com>
2022-02-19 13:58:47 +0300

    refactor User to entity view with annotations and relations

--- src/main/java/com/amr/project/model/entity/User.java
+++ src/main/java/com/amr/project/model/entity/User.java
@@ -10,7 +10,13 @@ import lombok.ToString;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import java.util.Calendar;
 import java.util.Collection;
@@ -28,29 +34,77 @@ public class User implements UserDetails {
     // нужны-ли они, возможно где-то вместо связи с ентити использовать id,
     // иначе есть вероятность попасть в констрейнты и не отстроить нормальную структуру
     // для взаимодействия с БД
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column
     private String email;
+
+    @Column
     private String username;
+
+    @Column
     private String password;
+
+    @Column
     private boolean activate;
+
+    @Column
     private String activationCode;
+
+    @Column
     private String phone;
+
+    @Column
     private String firstName;
+
+    @Column
     private String lastName;
+
+    @Column
     private int age;
+
+    @OneToMany
     private List<Address> address;
+
+    @Column
     private Role roles;
+
+    @Column
     private Gender gender;
+
+    @Column
     private Calendar birthday;
+
+    @OneToOne
     private Image images;
+
+    @OneToMany
     private List<Coupon> coupons;
+
+    @OneToMany
     private List<CartItem> cart;
+
+    @OneToMany
     private List<Order> orders;
+
+    @OneToMany
     private List<Review> reviews;
+
+    @OneToMany
     private List<Shop> shops;
+
+    @OneToOne
     private Favorite favorite;
+
+    @OneToMany
     private List<Discount> discounts;
+
+    @Column
     private boolean isUsingTwoFactorAuth;
+
+    @Column
     private String secret;
 
     @Override
