bf9bb713c7b8f6e4abb670d478de36a44091185f
Kris <kitty_kris@bk.ru>
2022-02-22 23:00:48 +0300

    add method for mapping list of Shop to ShopDto

--- src/main/java/com/amr/project/converter/MapStructMapper.java
+++ src/main/java/com/amr/project/converter/MapStructMapper.java
@@ -4,11 +4,14 @@ import com.amr.project.model.dto.*;
 import com.amr.project.model.entity.*;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface MapStructMapper {
 
     ShopDto shopToShopDto(Shop shop);
     Shop shopDtoToShop(ShopDto shopDto);
+    List<ShopDto> allShopsToShopsDto(List<Shop> shopList);
 
     ItemDto itemToItemDto(Item item);
     Item itemDtoToItem(ItemDto itemDto);
