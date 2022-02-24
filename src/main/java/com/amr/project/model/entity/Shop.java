4e2dddcfb94317c35867c367254ea611c4006212
Andrey Barantsov <andrey.barantsov75@gmail.com>
2022-02-25 00:33:23 +0300

    do some code refactoring of Shop with Image relationship

--- src/main/java/com/amr/project/model/entity/Shop.java
+++ src/main/java/com/amr/project/model/entity/Shop.java
@@ -3,7 +3,15 @@ package com.amr.project.model.entity;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import java.util.List;
 
@@ -12,21 +20,56 @@ import java.util.List;
 @Data
 @Builder
 public class Shop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String name;
+
+    @Column
     private String email;
+
+    @Column
     private String phone;
+
+    @Column
     private String description;
+
     private Country location;
+
     private List<Item> items;
+
     private List<Review> reviews;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "image_id")
     private Image logo;
+
+    @Column
     private int count;
+
+    @Column
     private double rating;
+
     private User user;
+
     private List<Discount> discounts;
+
+    @Column(name = "is_moderated")
     private boolean isModerated = false;
+
+    @Column(name = "is_moderate_accept")
     private boolean isModerateAccept = false;
+
+    @Column(name = "moderated_reject_reason")
     private String moderatedRejectReason;
+
+    @Column(name = "is_pretendent_to_be_deleted")
     private boolean isPretendentToBeDeleted = false;
+
+    public Shop() {
+
+    }
 }
