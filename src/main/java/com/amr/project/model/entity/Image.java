0bbd35c07d6226a14a375db399e5b6992f333403
Andrey Barantsov <andrey.barantsov75@gmail.com>
2022-02-25 00:19:49 +0300

    refactor User and Image

--- src/main/java/com/amr/project/model/entity/Image.java
+++ src/main/java/com/amr/project/model/entity/Image.java
@@ -1,10 +1,14 @@
 package com.amr.project.model.entity;
 
-
 import lombok.Builder;
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "image")
@@ -15,8 +19,10 @@ public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column
     private byte[] picture;
+
     @Column
     private Boolean isMain = false;
 
