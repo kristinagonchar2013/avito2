01b9d0297499554794fb4144584d456f7ec2f557
Andrey Barantsov <andrey.barantsov75@gmail.com>
2022-02-24 21:08:38 +0300

    refactor Country.java

--- src/main/java/com/amr/project/model/entity/Country.java
+++ src/main/java/com/amr/project/model/entity/Country.java
@@ -3,7 +3,16 @@ package com.amr.project.model.entity;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.List;
 
 @Entity
@@ -15,13 +24,12 @@ public class Country {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column
     private String name;
 
-    @OneToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "city_id")
-    private List <City> city;
-
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "country")
+    private List<City> city;
 
     public Country() {
 
