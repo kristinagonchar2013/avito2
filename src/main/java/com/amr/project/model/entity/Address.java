08a33523949c793e6dcac5cd07130ddbf946fe5c
Andrey Barantsov <andrey.barantsov75@gmail.com>
2022-02-24 22:07:03 +0300

    refactor Address.java

--- src/main/java/com/amr/project/model/entity/Address.java
+++ src/main/java/com/amr/project/model/entity/Address.java
@@ -3,7 +3,15 @@ package com.amr.project.model.entity;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "address")
@@ -13,13 +21,15 @@ public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column
+
+    @Column(name = "city_index")
     private String cityIndex;
+
     @Column
     private String street;
+
     @Column
     private String house;
-
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "city_id")
@@ -28,7 +38,6 @@ public class Address {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
-
 
     public Address() {
 
