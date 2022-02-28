f033fcdbdb603644f302b5e0880c14c3f068a2b9
Andrey Barantsov <andrey.barantsov75@gmail.com>
2022-02-28 19:41:06 +0300

    do code stile refactoring in Category.java

--- src/main/java/com/amr/project/model/entity/Category.java
+++ src/main/java/com/amr/project/model/entity/Category.java
@@ -3,7 +3,12 @@ package com.amr.project.model.entity;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "category")
@@ -13,6 +18,7 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column
     private String name;
 
