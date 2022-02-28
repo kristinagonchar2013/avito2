cece35089729fe4331f473d3b3572df3ed1af089
stas3252 <stanislav.kosyy@yandex.ru>
2022-02-28 13:29:07 +0300

    make RESTforCRUDitemInShop

--- src/main/java/com/amr/project/webapp/controller/CRUDItemInShopRESTController.java
+++ src/main/java/com/amr/project/webapp/controller/CRUDItemInShopRESTController.java
@@ -1,9 +1,12 @@
 package com.amr.project.webapp.controller;
 
+import com.amr.project.converter.MapStructMapper;
+import com.amr.project.model.dto.ItemDto;
 import com.amr.project.model.entity.Item;
 import com.amr.project.model.entity.Shop;
 import com.amr.project.service.abstracts.ReadWriteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,18 +16,21 @@ import javax.validation.Valid;
 @RequestMapping("/api/shop")
 public class CRUDItemInShopRESTController {
 
-    private final ReadWriteService shopService;
-    private final Converter converter;
+    private final ReadWriteService<Item, Long> itemService;
+    private final ReadWriteService<Shop, Long> shopService;
+    private final MapStructMapper mapper;
 
     @Autowired
-    public CRUDItemInShopRESTController(ReadWriteService shopService, Converter converter) {
+    public CRUDItemInShopRESTController(ReadWriteService<Item, Long> itemService, ReadWriteService<Shop, Long> shopService,
+                                        MapStructMapper mapper) {
         this.shopService = shopService;
-        this.converter = converter;
+        this.itemService = itemService;
+        this.mapper = mapper;
     }
 
     @PostMapping("/{id}/item")
     public ResponseEntity<ItemDto> addItemInShop(@Valid @RequestBody ItemDto itemDto, @PathVariable Long id) {
-        Item item = converter.itemDtoToItem(itemDto);
+        Item item = mapper.itemDtoToItem(itemDto);
         Shop shop = (Shop) shopService.findById(id);
         shop.addItem(item);
         shopService.persist(shop);
@@ -33,14 +39,15 @@ public class CRUDItemInShopRESTController {
 
     @PutMapping("/item")
     public ResponseEntity<ItemDto> updateItemInShop(@Valid @RequestBody ItemDto itemDto, @PathVariable Long id) {
-        Item item = converter.convetToItem(itemDto);
-
-        return ResponseEntity.ok(shopService.update(item));
+        Item item = mapper.itemDtoToItem(itemDto);
+        itemService.update(item);
+        return ResponseEntity.ok(itemDto);
     }
 
     @DeleteMapping("/item/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-
+        Item item = itemService.findById(id);
+        itemService.delete(item);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
