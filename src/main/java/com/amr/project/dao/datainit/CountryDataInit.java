e99d4d70b55409cb5f8f39f335643204401720dc
Kris <kitty_kris@bk.ru>
2022-03-01 02:55:42 +0300

    add classes for city and country data initializing on app startup

---
+++ src/main/java/com/amr/project/dao/datainit/CountryDataInit.java
@@ -0,0 +1,28 @@
+package com.amr.project.dao.datainit;
+
+import com.amr.project.dao.abstracts.CountryDao;
+import com.amr.project.model.entity.Country;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class CountryDataInit {
+
+    private final CountryDao countryDao;
+
+    @Autowired
+    public CountryDataInit(CountryDao countryDao) {
+        this.countryDao = countryDao;
+    }
+
+    @PostConstruct
+    public void countryDataInit() {
+        countryDao.persist(Country.builder().id(1L).name("Russia").build());
+        countryDao.persist(Country.builder().id(2L).name("Ukraine").build());
+        countryDao.persist(Country.builder().id(3L).name("Italy").build());
+        countryDao.persist(Country.builder().id(4L).name("England").build());
+        countryDao.persist(Country.builder().id(5L).name("USA").build());
+    }
+}
