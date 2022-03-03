d7a1c277a59e6dc8ccc59e114b656690affd6257
Andrey Barantsov <andrey.barantsov75@gmail.com>
2022-03-03 11:20:02 +0300

    remove * import in Item

--- src/main/java/com/amr/project/model/entity/Item.java
+++ src/main/java/com/amr/project/model/entity/Item.java
@@ -3,7 +3,17 @@ package com.amr.project.model.entity;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Set;
