18ff2de7cfc0fa4e914c96d6e9b3928148c9e4c8
Kris <kitty_kris@bk.ru>
2022-03-07 09:00:30 +0300

    change user data init

--- src/main/java/com/amr/project/datainit/UserDataInit.java
+++ src/main/java/com/amr/project/datainit/UserDataInit.java
@@ -34,28 +34,44 @@ public class UserDataInit {
 
     @PostConstruct
     private void userDataInit() {
-        userDao.persist(User.builder().id(1L).email("user@init.com").username("user").password("user").activate(true)
-                .activationCode("user").phone("1111111").firstName("user").lastName("user").age(30).gender(Gender.MALE)
-                .address(Arrays.asList(addressDao.findById(1L), addressDao.findById(2L)))
-                .roles(Arrays.asList(Role.USER)).birthday(new GregorianCalendar(1991, Calendar.MAY, 9))
-                .orders(Arrays.asList(orderDao.findById(1L), orderDao.findById(2L))).favorite(favoriteDao.findById(5L))
-                .discounts(Arrays.asList(discountDao.findById(1L), discountDao.findById(4L))).build());
-        userDao.persist(User.builder().id(2L).email("admin@init.com").username("admin").password("admin").activate(true)
-                .activationCode("admin").phone("1111111").firstName("admin").lastName("admin").age(30).gender(Gender.MALE)
-                .roles(Arrays.asList(Role.ADMINISTRATOR)).birthday(new GregorianCalendar(1991, Calendar.MAY, 9))
-                .shops(Arrays.asList(shopDao.findById(1L))).build());
-        userDao.persist(User.builder().id(3L).email("moderator@init.com").username("moderator").password("moderator")
-                .activate(true).activationCode("moderator").phone("1111111").firstName("moderator").lastName("moderator")
-                .age(30).gender(Gender.MALE).roles(Arrays.asList(Role.MODERATOR))
+        userDao.persist(User.builder().id(1L).email("user@init.com")
+                .username("user").password("user")
+                .activate(true).activationCode("user")
+                .phone("1111111").firstName("user").lastName("user").age(30).gender(Gender.MALE)
+                .role(Role.USER)
+                .birthday(new GregorianCalendar(1991, Calendar.MAY, 9))
+//                .address(Arrays.asList(addressDao.findById(1L), addressDao.findById(2L)))
+//                .orders(Arrays.asList(orderDao.findById(1L), orderDao.findById(2L))).favorite(favoriteDao.findById(5L))
+//                .discounts(Arrays.asList(discountDao.findById(1L), discountDao.findById(4L)))
+                .build());
+
+        userDao.persist(User.builder().id(2L).email("admin@init.com")
+                .username("admin").password("admin")
+                .activate(true).activationCode("admin")
+                .phone("1111111").firstName("admin").lastName("admin").age(30).gender(Gender.MALE)
+                .role(Role.ADMINISTRATOR)
+                .birthday(new GregorianCalendar(1991, Calendar.MAY, 9))
+//                .shops(Arrays.asList(shopDao.findById(1L)))
+                .build());
+
+        userDao.persist(User.builder().id(3L).email("moderator@init.com")
+                .username("moderator").password("moderator")
+                .activate(true).activationCode("moderator")
+                .phone("1111111").firstName("moderator").lastName("moderator").age(30).gender(Gender.MALE)
+                .role(Role.MODERATOR)
                 .birthday(new GregorianCalendar(1991, Calendar.MAY, 9))
                 .build());
-        userDao.persist(User.builder().id(4L).email("user1@init.com").username("user1").password("user1").activate(true)
-                .activationCode("user1").phone("1111111").firstName("user1").lastName("user1").age(30).gender(Gender.MALE)
-                .address(Arrays.asList(addressDao.findById(3L), addressDao.findById(4L)))
-                .roles(Arrays.asList(Role.USER, Role.ADMINISTRATOR))
+
+        userDao.persist(User.builder().id(4L).email("user1@init.com")
+                .username("user1").password("user1")
+                .activate(true).activationCode("user1")
+                .phone("1111111").firstName("user1").lastName("user1").age(30).gender(Gender.MALE)
+                .role(Role.USER)
                 .birthday(new GregorianCalendar(1991, Calendar.MAY, 9))
-                .orders(Arrays.asList(orderDao.findById(3L), orderDao.findById(4L))).favorite(favoriteDao.findById(1L))
-                .shops(Arrays.asList(shopDao.findById(2L), shopDao.findById(3L)))
-                .discounts(Arrays.asList(discountDao.findById(3L), discountDao.findById(4L))).build());
+//                .address(Arrays.asList(addressDao.findById(3L), addressDao.findById(4L)))
+//                .orders(Arrays.asList(orderDao.findById(3L), orderDao.findById(4L))).favorite(favoriteDao.findById(1L))
+//                .shops(Arrays.asList(shopDao.findById(2L), shopDao.findById(3L)))
+//                .discounts(Arrays.asList(discountDao.findById(3L), discountDao.findById(4L)))
+                .build());
     }
 }
