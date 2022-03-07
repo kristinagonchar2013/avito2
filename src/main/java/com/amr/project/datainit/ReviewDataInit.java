0c1ab38be2ad95f4bc667c83115dc71418b166b5
Kris <kitty_kris@bk.ru>
2022-03-07 08:38:05 +0300

    change review data init

--- src/main/java/com/amr/project/datainit/ReviewDataInit.java
+++ src/main/java/com/amr/project/datainit/ReviewDataInit.java
@@ -1,6 +1,9 @@
 package com.amr.project.datainit;
 
+import com.amr.project.dao.abstracts.ItemDao;
 import com.amr.project.dao.abstracts.ReviewDao;
+import com.amr.project.dao.abstracts.ShopDao;
+import com.amr.project.dao.abstracts.UserDao;
 import com.amr.project.model.entity.Review;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,43 +15,115 @@ import java.util.Date;
 public class ReviewDataInit {
 
     private final ReviewDao reviewDao;
+    private final ItemDao itemDao;
+    private final ShopDao shopDao;
+    private final UserDao userDao;
 
     @Autowired
-    private ReviewDataInit(ReviewDao reviewDao) {
+    private ReviewDataInit(ReviewDao reviewDao, ItemDao itemDao, ShopDao shopDao, UserDao userDao) {
         this.reviewDao = reviewDao;
+        this.itemDao = itemDao;
+        this.shopDao = shopDao;
+        this.userDao = userDao;
     }
 
     @PostConstruct
     private void reviewDataInit() {
-        reviewDao.persist(Review.builder().id(1L).dignity("it's ok").date(new Date()).rating(4).isModerated(true)
+        reviewDao.persist(Review.builder().id(1L)
+                .dignity("it's ok").flaw("Too simple")
+                .text("i like this")
+                .date(new Date()).rating(4)
+                .isModerateAccept(true).moderatedRejectReason("adding")
+                .item(itemDao.findById(1L))
+                .user(userDao.findById(1L))
                 .build());
-        reviewDao.persist(Review.builder().id(2L).dignity("it's not ok").date(new Date()).rating(5).isModerated(true)
+
+        reviewDao.persist(Review.builder().id(2L)
+                .dignity("it's not ok").flaw("don't like")
+                .date(new Date()).rating(5)
+                .isModerateAccept(true).moderatedRejectReason("adding")
+                .item(itemDao.findById(2L))
+                .user(userDao.findById(1L))
                 .build());
-        reviewDao.persist(Review.builder().id(3L).dignity("ugly").date(new Date()).rating(3).isModerated(true)
+
+        reviewDao.persist(Review.builder().id(3L)
+                .dignity("ugly").flaw("all")
+                .date(new Date()).rating(2)
+                .isModerateAccept(true).moderatedRejectReason("adding")
+                .item(itemDao.findById(3L))
+                .user(userDao.findById(1L))
                 .build());
-        reviewDao.persist(Review.builder().id(4L).dignity("maybe").date(new Date()).rating(4).isModerated(true)
+
+        reviewDao.persist(Review.builder().id(4L)
+                .dignity("maybe").flaw("maybe")
+                .date(new Date()).rating(4)
+                .isModerateAccept(true).moderatedRejectReason("adding")
+                .item(itemDao.findById(4L))
+                .user(userDao.findById(2L))
                 .build());
-        reviewDao.persist(Review.builder().id(5L).dignity("cool").date(new Date()).rating(5).isModerated(true)
+
+        reviewDao.persist(Review.builder().id(5L)
+                .dignity("cool").flaw("no")
+                .date(new Date()).rating(5)
+                .isModerateAccept(true).moderatedRejectReason("adding")
+                .shop(shopDao.findById(1L))
+                .user(userDao.findById(3L))
                 .build());
-        reviewDao.persist(Review.builder().id(6L).dignity("bad").date(new Date()).rating(3).isModerated(true)
+
+        reviewDao.persist(Review.builder().id(6L)
+                .dignity("bad").flaw("very bad")
+                .date(new Date()).rating(3)
+                .isModerateAccept(true).moderatedRejectReason("adding")
+                .item(itemDao.findById(5L))
+                .user(userDao.findById(4L))
                 .build());
-        reviewDao.persist(Review.builder().id(7L).dignity("good").date(new Date()).rating(4).isModerated(true)
+
+        reviewDao.persist(Review.builder().id(7L)
+                .dignity("good").flaw("no")
+                .date(new Date()).rating(4)
+                .isModerateAccept(true).moderatedRejectReason("adding")
+                .item(itemDao.findById(5L))
+                .user(userDao.findById(1L))
                 .build());
-        reviewDao.persist(Review.builder().id(8L).dignity("i got it").date(new Date()).rating(5).isModerated(true)
+
+        reviewDao.persist(Review.builder().id(8L)
+                .dignity("i got it").flaw("no")
+                .date(new Date()).rating(5)
+                .isModerateAccept(true).moderatedRejectReason("adding")
+                .item(itemDao.findById(7L))
+                .user(userDao.findById(2L))
                 .build());
-        reviewDao.persist(Review.builder().id(9L).dignity("very bad").date(new Date()).rating(2).isModerated(true)
+
+        reviewDao.persist(Review.builder().id(9L)
+                .dignity("very bad").flaw("it all")
+                .date(new Date()).rating(2)
+                .isModerateAccept(true).moderatedRejectReason("adding")
+                .shop(shopDao.findById(2L))
+                .user(userDao.findById(3L))
                 .build());
-        reviewDao.persist(Review.builder().id(10L).dignity("good staff").date(new Date()).rating(5).isModerated(true)
+
+        reviewDao.persist(Review.builder().id(10L)
+                .dignity("good staff").flaw("no")
+                .date(new Date()).rating(5)
+                .isModerateAccept(true).moderatedRejectReason("adding")
+                .item(itemDao.findById(7L))
+                .user(userDao.findById(4L))
                 .build());
-        reviewDao.persist(Review.builder().id(11L).dignity("normal").date(new Date()).rating(4).isModerated(true)
+
+        reviewDao.persist(Review.builder().id(11L)
+                .dignity("normal").flaw("its simple")
+                .date(new Date()).rating(4)
+                .isModerateAccept(true).moderatedRejectReason("adding")
+                .shop(shopDao.findById(1L))
+                .user(userDao.findById(1L))
                 .build());
-        reviewDao.persist(Review.builder().id(12L).dignity("sadly").date(new Date()).rating(3).isModerated(true)
-                .build());
-        reviewDao.persist(Review.builder().id(13L).dignity("very very bad").date(new Date()).rating(2).isModerated(true)
-                .build());
-        reviewDao.persist(Review.builder().id(14L).dignity("super").date(new Date()).rating(5).isModerated(true)
-                .build());
-        reviewDao.persist(Review.builder().id(15L).dignity("good").date(new Date()).rating(4).isModerated(true)
+
+        reviewDao.persist(Review.builder().id(12L)
+                .dignity("no").flaw("sadly")
+                .date(new Date()).rating(3)
+                .isModerateAccept(true).moderatedRejectReason("adding")
+                .shop(shopDao.findById(4L))
+                .user(userDao.findById(4L))
                 .build());
     }
 }
