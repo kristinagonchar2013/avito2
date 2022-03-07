fae3dd00a3ced53062a9ad8e50431b4e116e56ab
Kris <kitty_kris@bk.ru>
2022-03-07 08:50:35 +0300

    change shop data init

--- src/main/java/com/amr/project/datainit/ShopDataInit.java
+++ src/main/java/com/amr/project/datainit/ShopDataInit.java
@@ -7,46 +7,62 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class ShopDataInit {
 
     private final ShopDao shopDao;
     private final CityDao cityDao;
-    private final ItemDao itemDao;
-    private final ReviewDao reviewDao;
+    private final UserDao userDao;
     private final DiscountDao discountDao;
+    private final CouponDao couponDao;
 
     @Autowired
-    private ShopDataInit(ShopDao shopDao, CityDao cityDao, ItemDao itemDao, ReviewDao reviewDao, DiscountDao discountDao) {
+    public ShopDataInit(ShopDao shopDao, CityDao cityDao, UserDao userDao, DiscountDao discountDao, CouponDao couponDao) {
         this.shopDao = shopDao;
         this.cityDao = cityDao;
-        this.itemDao = itemDao;
-        this.reviewDao = reviewDao;
+        this.userDao = userDao;
         this.discountDao = discountDao;
+        this.couponDao = couponDao;
     }
 
     @PostConstruct
     private void shopDataInit() {
-        shopDao.persist(Shop.builder().id(1L).name("Chairs & Co").location(cityDao.findById(1L))
-                .email("chairs.co@gmail.com").phone("11111111").rating(4.44)
-                .items(Arrays.asList(itemDao.findById(7L), itemDao.findById(8L))).isModerated(true)
-                .reviews(Arrays.asList(reviewDao.findById(15L)))
-                .discounts(Arrays.asList(discountDao.findById(1L))).build());
-        shopDao.persist(Shop.builder().id(2L).name("Mr.Table").location(cityDao.findById(2L))
-                .email("table.and.brothers@gmail.com").phone("22222222").rating(5.00).isModerated(true)
-                .items(Arrays.asList(itemDao.findById(1L), itemDao.findById(2L)))
-                .reviews(Arrays.asList(reviewDao.findById(14L)))
-                .discounts(discountDao.findAll()).build());
-        shopDao.persist(Shop.builder().id(3L).name("Active Sport Shop").location(cityDao.findById(3L))
-                .email("sportforever.com").phone("333333333").rating(4.89).isModerated(true)
-                .items(Arrays.asList(itemDao.findById(5L), itemDao.findById(6L)))
-                .reviews(Arrays.asList(reviewDao.findById(13L)))
-                .discounts(Arrays.asList(discountDao.findById(2L), discountDao.findById(4L))).build());
-        shopDao.persist(Shop.builder().id(4L).name("Only Mobile").location(cityDao.findById(4L))
-                .email("worldofmobile.net").email("444444444").rating(3.2).isModerated(true)
-                .items(Arrays.asList(itemDao.findById(3L), itemDao.findById(4L)))
-                .reviews(Arrays.asList(reviewDao.findById(12L)))
-                .discounts(Arrays.asList(discountDao.findById(3L))).build());
+        shopDao.persist(Shop.builder().id(1L).name("Chairs & Co")
+                .email("chairs.co@gmail.com").phone("11111111")
+                .description("You will find what you want").rating(4.44)
+                .isModerated(true).isModerateAccept(true).moderatedRejectReason("adding")
+                .location(cityDao.findById(1L))
+                .user(userDao.findById(1L))
+                .discounts(List.of(discountDao.findById(1L)))
+                .coupons(List.of(couponDao.findById(1L), couponDao.findById(4L), couponDao.findById(7L))).build());
+
+        shopDao.persist(Shop.builder().id(2L).name("Mr.Table")
+                .email("table.and.brothers@gmail.com").phone("22222222")
+                .description("It's best of the best of the best").rating(5.00)
+                .isModerated(true).isModerateAccept(true).moderatedRejectReason("adding")
+                .location(cityDao.findById(2L))
+                .user(userDao.findById(1L))
+                .discounts(discountDao.findAll())
+                .coupons(List.of(couponDao.findById(2L), couponDao.findById(5L))).build());
+
+        shopDao.persist(Shop.builder().id(3L).name("Active Sport Shop")
+                .email("sportforever.com").phone("333333333")
+                .description("We love sport").rating(4.89)
+                .isModerated(true).isModerateAccept(true).moderatedRejectReason("adding")
+                .location(cityDao.findById(3L))
+                .user(userDao.findById(4L))
+                .discounts(List.of(discountDao.findById(2L), discountDao.findById(4L)))
+                .coupons(List.of(couponDao.findById(3L), couponDao.findById(6L))).build());
+
+        shopDao.persist(Shop.builder().id(4L).name("Only Mobile")
+                .email("worldofmobile.net").email("444444444")
+                .description("We call you!").rating(3.2)
+                .isModerated(true).isModerateAccept(true).moderatedRejectReason("adding")
+                .location(cityDao.findById(4L))
+                .user(userDao.findById(4L))
+                .discounts(List.of(discountDao.findById(3L)))
+                .build());
     }
 }
