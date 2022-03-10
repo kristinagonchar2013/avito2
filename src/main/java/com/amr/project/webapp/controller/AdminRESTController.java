26470edc3d53e9c7e23cc6743d0dbc0268257262
stas3252 <stanislav.kosyy@yandex.ru>
2022-03-10 16:58:12 +0300

    shortened the code

--- src/main/java/com/amr/project/webapp/controller/AdminRESTController.java
+++ src/main/java/com/amr/project/webapp/controller/AdminRESTController.java
@@ -1,59 +1,39 @@
 package com.amr.project.webapp.controller;
 
-import com.amr.project.converter.ItemMapper;
-import com.amr.project.converter.ShopMapper;
-import com.amr.project.model.dto.ItemDto;
-import com.amr.project.model.dto.ShopDto;
-import com.amr.project.model.entity.Item;
-import com.amr.project.model.entity.Shop;
 import com.amr.project.service.abstracts.ItemService;
 import com.amr.project.service.abstracts.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminRESTController {
     private final ShopService shopService;
     private final ItemService itemService;
-    private final ShopMapper shopMapper;
-    private final ItemMapper itemMapper;
 
     @Autowired
-    AdminRESTController(ShopService shopService, ItemService itemService, ShopMapper shopMapper, ItemMapper itemMapper) {
+    AdminRESTController(ShopService shopService, ItemService itemService) {
         this.shopService = shopService;
         this.itemService = itemService;
-        this.shopMapper = shopMapper;
-        this.itemMapper = itemMapper;
     }
 
-    @PostMapping("/approve/shop/{id}")
-    public ResponseEntity<ShopDto> approveShop(@PathVariable Long id) {
-        Shop shop = shopService.findById(id);
-        shop.setStatus("approve");
-        return ResponseEntity.ok(shopMapper.shopToShopDto(shop));
-    }
+    @PostMapping("/setStatus")
+    public ResponseEntity approveShop(@RequestParam Map<String,String> requestParams) {
 
-    @PostMapping("/notApprove/shop/{id}")
-    public ResponseEntity<ShopDto> notApproveShop(@PathVariable Long id) {
-        Shop shop = shopService.findById(id);
-        shop.setStatus("notApprove");
-        return ResponseEntity.ok(shopMapper.shopToShopDto(shop));
-    }
+        String nameEntity = requestParams.get("nameEntity");
+        String id = requestParams.get("id");
+        String status = requestParams.get("status");
 
-    @PostMapping("/approve/item/{id}")
-    public ResponseEntity<ItemDto> approveItem(@PathVariable Long id) {
-        Item item = itemService.findById(id);
-        item.setStatus("approve");
-        return ResponseEntity.ok(itemMapper.itemToItemDto(item));
-    }
+        if (nameEntity.equals("shop")) {
+            shopService.findById(Long.parseLong(id)).setStatus(status);
+        } else if (nameEntity.equals("item")) {
+            itemService.findById(Long.parseLong(id)).setStatus(status);
+        }
 
-    @PostMapping("/notApprove/item/{id}")
-    public ResponseEntity<ItemDto> notApproveItem(@PathVariable Long id) {
-        Item item = itemService.findById(id);
-        item.setStatus("notApprove");
-        return ResponseEntity.ok(itemMapper.itemToItemDto(item));
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
-
 }
