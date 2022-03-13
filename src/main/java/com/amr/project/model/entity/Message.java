89c74bdcaff9868be5849e20935ac77eb957428b
Kris <kitty_kris@bk.ru>
2022-03-13 17:15:33 +0300

    change field "timestamp" on field "localDateTime" delete not user annotations add lombok annotation for no arg constructor

--- src/main/java/com/amr/project/model/entity/Message.java
+++ src/main/java/com/amr/project/model/entity/Message.java
@@ -1,8 +1,10 @@
 package com.amr.project.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,16 +14,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "message")
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,24 +33,17 @@ public class Message {
     private String textMessage;
     private boolean viewed;
 
-    @Column(name = "time_stamp")
-    private Timestamp timeStamp;
+    @Column(name = "creation_time", updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime creationTime;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "chat_id")
     private Chat chat;
 
     @OneToOne(fetch = FetchType.LAZY)
-//    @MapsId
-//    @JoinColumn(name = "user_id")
     private User to;
 
     @OneToOne(fetch = FetchType.LAZY)
-//    @MapsId
-//    @JoinColumn(name = "user_id")
     private User from;
-
-    public Message() {
-        this.timeStamp = new Timestamp(System.currentTimeMillis());
-    }
 }
