5d48209fc6bd0f6445a6555ca30ea18c19303e24
Kris <kitty_kris@bk.ru>
2022-03-14 00:41:04 +0300

    add method in message mapper for mapping list of messages to list of message dto

--- src/main/java/com/amr/project/converter/MessageMapper.java
+++ src/main/java/com/amr/project/converter/MessageMapper.java
@@ -4,9 +4,12 @@ import com.amr.project.model.dto.MessageDto;
 import com.amr.project.model.entity.Message;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface MessageMapper {
 
     Message messageDtoToMessage(MessageDto messageDto);
     MessageDto messageToMessageDto(Message message);
+    List<MessageDto> listOfMessageToMessageDto(List<Message> list);
 }
