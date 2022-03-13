e97697de2a0d7fe21b251e1a44ae0d3a80b03416
Kris <kitty_kris@bk.ru>
2022-03-14 00:40:18 +0300

    add method in chat mapper for mapping set of chats fo chats dto

--- src/main/java/com/amr/project/converter/ChatMapper.java
+++ src/main/java/com/amr/project/converter/ChatMapper.java
@@ -4,9 +4,12 @@ import com.amr.project.model.dto.ChatDto;
 import com.amr.project.model.entity.Chat;
 import org.mapstruct.Mapper;
 
+import java.util.Set;
+
 @Mapper(componentModel = "spring")
 public interface ChatMapper {
 
     Chat chatDtoToChat(ChatDto chatDto);
     ChatDto chatToChatDto(Chat chat);
+    Set<ChatDto> setOfChatToChatDto(Set<Chat> chatSet);
 }
