928bc5dfb52f4dc18b74dd46ed9a5352148f4e5a
Kris <kitty_kris@bk.ru>
2022-03-16 21:14:04 +0300

    change destination for simple broker

--- src/main/java/com/amr/project/webapp/config/websocket/WebSocketConfig.java
+++ src/main/java/com/amr/project/webapp/config/websocket/WebSocketConfig.java
@@ -11,14 +11,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/project");
+        registry.setUserDestinationPrefix("/user");
+    }
+    @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocket");
         registry.addEndpoint("/websocket").withSockJS();
-    }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/project").enableSimpleBroker("/user");
-        registry.setUserDestinationPrefix("/user");
     }
 }
