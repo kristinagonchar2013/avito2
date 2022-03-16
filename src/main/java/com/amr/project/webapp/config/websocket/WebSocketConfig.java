f3f79997647cefd2b8610d791cf16b0574263861
Kris <kitty_kris@bk.ru>
2022-03-17 01:42:00 +0300

    change name of destination prefix

--- src/main/java/com/amr/project/webapp/config/websocket/WebSocketConfig.java
+++ src/main/java/com/amr/project/webapp/config/websocket/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/project");
+        registry.setApplicationDestinationPrefixes("/avito2");
         registry.setUserDestinationPrefix("/user");
     }
     @Override
