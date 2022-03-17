7b326e0e66bdb7a92a2c69d3d64d8ac609801fb5
gvandek <gvandek.kim@gmail.com>
2022-03-18 01:00:15 +1100

    created method delete in ModeratorController

--- src/main/java/com/amr/project/webapp/controller/ModeratorController.java
+++ src/main/java/com/amr/project/webapp/controller/ModeratorController.java
@@ -1,30 +1,61 @@
 package com.amr.project.webapp.controller;
 
 import com.amr.project.converter.UserMapper;
+import com.amr.project.model.dto.UserDto;
+import com.amr.project.model.entity.Item;
+import com.amr.project.model.entity.Shop;
 import com.amr.project.model.entity.User;
+import com.amr.project.service.abstracts.ItemService;
+import com.amr.project.service.abstracts.ShopService;
 import com.amr.project.service.abstracts.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
+@RestController
+@RequestMapping("/moderator")
 public class ModeratorController {
-    private UserMapper userMapper;
-    private UserService userService;
+    private final UserMapper userMapper;
+    private final UserService userService;
+    private final ShopService shopService;
+    private final ItemService itemService;
 
     @Autowired
-    public UserAdminController(UserMapper userMapper, UserService userService) {
+    public ModeratorController(UserMapper userMapper, UserService userService, ShopService shopService, ItemService itemService) {
         this.userMapper = userMapper;
         this.userService = userService;
+        this.shopService = shopService;
+        this.itemService = itemService;
     }
 
-    @DeleteMapping("/{id}")
-    ResponseEntity deleteUserById(@PathVariable Long id) {
-        User user = userService.findById(id);
-        user.setPretendedToBeDeleted(true);
-            userService.deleteByIdCascadeEnable(id);
-            return ResponseEntity.status(HttpStatus.OK).build();
+    @DeleteMapping("/delete")
+    ResponseEntity deleteById(@RequestParam Map<String, String> request) {
+        String nameEntity = request.get("nameEntity");
+        String id = request.get("id");
+        if (nameEntity.equals("user")) {
+            User user = userService.findById(Long.parseLong(id));
+            user.setPretendedToBeDeleted(true);
+        } else if (nameEntity.equals("shop")) {
+            Shop shop = shopService.findById(Long.parseLong(id));
+            shop.setPretendentToBeDeleted(true);
+        } else if (nameEntity.equals("item")) {
+            Item item = itemService.findById(Long.parseLong(id));
+            item.setPretendedToBeDeleted(true);
+        }
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<UserDto> updateShop(@RequestBody Map<String, String> requestBody, @PathVariable Long id) {
+        User user = userMapper.userDtoToUser(userDto);
+        user.setId(id);
+        if (user.getPassword().isEmpty()) {
+            user.setPassword(userService.findById(id).getPassword());
+        }
+        userService.update(user);
+        return ResponseEntity.status(HttpStatus.OK).body(userMapper.userToUserDto(user));
     }
 }
