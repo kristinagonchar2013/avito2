cbeec32e7e8a8aa08500d5397a7cca8b3f8c75da
Andrey Barantsov <andrey.barantsov75@gmail.com>
2022-03-17 18:04:47 +0300

    add @lambok annotations to Email

--- src/main/java/com/amr/project/model/domain/Email.java
+++ src/main/java/com/amr/project/model/domain/Email.java
@@ -1,7 +1,9 @@
 package com.amr.project.model.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
@@ -9,6 +11,8 @@ import javax.sql.DataSource;
 @Component
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Email {
 
     private String[] to;
