1a2a88eaf8989fc757906f62a989ccfd8ace4f38
gvandek <gvandek.kim@gmail.com>
2022-03-18 01:38:59 +1100

    partially created update method of ModeratorController

--- src/main/java/com/amr/project/webapp/controller/ModeratorController.java
+++ src/main/java/com/amr/project/webapp/controller/ModeratorController.java
@@ -1,6 +1,10 @@
 package com.amr.project.webapp.controller;
 
+import com.amr.project.converter.ItemMapper;
+import com.amr.project.converter.ShopMapper;
 import com.amr.project.converter.UserMapper;
+import com.amr.project.model.dto.ItemDto;
+import com.amr.project.model.dto.ShopDto;
 import com.amr.project.model.dto.UserDto;
 import com.amr.project.model.entity.Item;
 import com.amr.project.model.entity.Shop;
@@ -11,6 +15,7 @@ import com.amr.project.service.abstracts.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -19,13 +24,17 @@ import java.util.Map;
 @RequestMapping("/moderator")
 public class ModeratorController {
     private final UserMapper userMapper;
+    private final ShopMapper shopMapper;
+    private final ItemMapper itemMapper;
     private final UserService userService;
     private final ShopService shopService;
     private final ItemService itemService;
 
     @Autowired
-    public ModeratorController(UserMapper userMapper, UserService userService, ShopService shopService, ItemService itemService) {
+    public ModeratorController(UserMapper userMapper, ShopMapper shopMapper, ItemMapper itemMapper, UserService userService, ShopService shopService, ItemService itemService) {
         this.userMapper = userMapper;
+        this.shopMapper = shopMapper;
+        this.itemMapper = itemMapper;
         this.userService = userService;
         this.shopService = shopService;
         this.itemService = itemService;
@@ -35,27 +44,50 @@ public class ModeratorController {
     ResponseEntity deleteById(@RequestParam Map<String, String> request) {
         String nameEntity = request.get("nameEntity");
         String id = request.get("id");
-        if (nameEntity.equals("user")) {
-            User user = userService.findById(Long.parseLong(id));
-            user.setPretendedToBeDeleted(true);
-        } else if (nameEntity.equals("shop")) {
-            Shop shop = shopService.findById(Long.parseLong(id));
-            shop.setPretendentToBeDeleted(true);
-        } else if (nameEntity.equals("item")) {
-            Item item = itemService.findById(Long.parseLong(id));
-            item.setPretendedToBeDeleted(true);
+        switch (nameEntity) {
+            case "user":
+                User user = userService.findById(Long.parseLong(id));
+                user.setPretendedToBeDeleted(true);
+                break;
+            case "shop":
+                Shop shop = shopService.findById(Long.parseLong(id));
+                shop.setPretendentToBeDeleted(true);
+                break;
+            case "item":
+                Item item = itemService.findById(Long.parseLong(id));
+                item.setPretendedToBeDeleted(true);
+                break;
         }
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @PutMapping("/edit/{id}")
-    public ResponseEntity<UserDto> updateShop(@RequestBody Map<String, String> requestBody, @PathVariable Long id) {
-        User user = userMapper.userDtoToUser(userDto);
-        user.setId(id);
-        if (user.getPassword().isEmpty()) {
-            user.setPassword(userService.findById(id).getPassword());
+    public ResponseEntity<?> update(@RequestBody Object dto, @RequestParam Map<String, String> request) {
+        String nameEntity = request.get("nameEntity");
+        String id = request.get("id");
+        String body;
+        switch (nameEntity) {
+            case "user":
+                User user = userMapper.userDtoToUser((UserDto) dto);
+                user.setId(Long.parseLong(id));
+                if (user.getPassword().isEmpty()) {
+                    user.setPassword(userService.findById(Long.parseLong(id)).getPassword());
+                }
+                userService.update(user);
+                return ResponseEntity.status(HttpStatus.OK).body(userMapper.userToUserDto(user));
+            case "shop":
+                Shop shop = shopMapper.shopDtoToShop((ShopDto) dto);
+                shop.setId(Long.parseLong(id));
+                shopService.update(shop);
+                body = String.valueOf(shopMapper.shopToShopDto(shop));
+//                return ResponseEntity.status(HttpStatus.OK).body(body);
+                break;
+            case "item":
+                Item item = itemMapper.itemDtoToItem((ItemDto) dto);
+                item.setId(Long.parseLong(id));
+                itemService.update(item);
+                return ResponseEntity.status(HttpStatus.OK).body(itemMapper.itemToItemDto(item));
         }
-        userService.update(user);
-        return ResponseEntity.status(HttpStatus.OK).body(userMapper.userToUserDto(user));
+        return ResponseEntity.status(HttpStatus.OK).body(body);
     }
 }
