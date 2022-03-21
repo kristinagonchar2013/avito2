6dc85716de727742b37757acc625a078ae93d1b0
redhotrocknrolla <Jimbo_jo@mail.ru>
2022-03-21 19:23:55 +0300

    MainPage join

--- src/main/java/com/amr/project/webapp/controller/MainPageController.java
+++ src/main/java/com/amr/project/webapp/controller/MainPageController.java
@@ -8,22 +8,15 @@ import com.amr.project.model.entity.Shop;
 import com.amr.project.model.entity.User;
 import com.amr.project.service.abstracts.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.awt.print.Pageable;
 import java.util.List;
 
 @RestController
 public class MainPageController {
-
 
     private final MainPageMapper mainPageMapper;
     private final ShopService shopService;
@@ -52,8 +45,12 @@ public class MainPageController {
         List<Shop> shopList = paginationShopService.getAllShops(pageNo, pageSize, sortBy);
         List <Item> itemList = paginationItemService.getAllItems(pageNo, pageSize, sortBy);
         List<Category> categoryList = categoryService.findAll();
-        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user;
+        if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof String) {
+            return ResponseEntity.ok().body(mainPageMapper.mainPageToMainPageDtoHead(shopList,itemList,categoryList,null));
+        } else {
+            user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        }
         return ResponseEntity.ok().body(mainPageMapper.mainPageToMainPageDtoHead(shopList,itemList,categoryList,user));
     }
-
 }
