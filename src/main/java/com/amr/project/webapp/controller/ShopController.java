d1536cf8a1eb64aea0ea1c7a5bb4482f387c1c53
Kris <kitty_kris@bk.ru>
2022-03-23 19:03:59 +0300

    add swagger annotations for controller methods in Shop Controller

--- src/main/java/com/amr/project/webapp/controller/ShopController.java
+++ src/main/java/com/amr/project/webapp/controller/ShopController.java
@@ -5,6 +5,9 @@ import com.amr.project.model.dto.ShopDto;
 import com.amr.project.model.entity.Shop;
 import com.amr.project.service.abstracts.ShopService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -28,26 +31,43 @@ public class ShopController {
     }
 
     @GetMapping
-    @Operation(description = "Вывод всех магазинов")
+    @Operation(summary = "Вывод всех магазинов")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK, возвращает список магазинов")
+    })
     public ResponseEntity<List<ShopDto>> findAll() {
         List<Shop> list = service.findAll();
         return ResponseEntity.ok(mapper.allShopsToShopsDto(list));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ShopDto> getOneShop(@PathVariable Long id) {
+    @Operation(summary = "Вывод магазина по его id")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK, возвращает один магазин")
+    })
+    public ResponseEntity<ShopDto> getOneShop(@Parameter(description = "Идентификатор пользователя")
+                                                  @PathVariable Long id) {
         Shop shop = service.findById(id);
         return ResponseEntity.ok(mapper.shopToShopDto(shop));
     }
 
     @PostMapping
+    @Operation(summary = "Создание магазина")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "CREATED, возвращает только созданный магазин")
+    })
     public ResponseEntity<ShopDto> createShop(@RequestBody ShopDto shopDto) {
         service.persist(mapper.shopDtoToShop(shopDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(shopDto);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ShopDto> updateShop(@RequestBody ShopDto shopDto, @PathVariable Long id) {
+    @Operation(summary = "Обновление магазина по его id")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK, возвращает обновленный магазин")
+    })
+    public ResponseEntity<ShopDto> updateShop(@RequestBody ShopDto shopDto
+            , @Parameter(description = "Идентификатор пользователя")  @PathVariable Long id) {
         Shop shop = mapper.shopDtoToShop(shopDto);
         shop.setId(id);
         service.update(shop);
@@ -55,7 +75,12 @@ public class ShopController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteShopById(@PathVariable Long id) {
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "возвращает только Http status \"OK\"")
+    })
+    @Operation(summary = "Удаление магазина по его id")
+    public ResponseEntity<Void> deleteShopById(@Parameter(description = "Идентификатор пользователя")
+                                                   @PathVariable Long id) {
         service.delete(service.findById(id));
         return ResponseEntity.status(HttpStatus.OK).build();
     }
